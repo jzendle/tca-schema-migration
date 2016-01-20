58755f7c80d107b33337fae25e84003f4f43e2d7
liebniz <joe_zendle@yahoo.com>
2016-01-20 12:06:51 -0700

    initial

---
+++ src/main/resources/canonical.sql
@@ -0,0 +1,41 @@
+SELECT 
+             DISTINCT 
+             
+             -- C.c_id, 
+             -- C.create_date, 
+             -- C.create_email, 
+             -- C.update_date, 
+             -- C.update_email, 
+             MA.m_id as ma_m_id,
+             MA.c_id as ma_c_id, 
+             MA.metric_id as ma_met_id,
+             C.circuit_id as circuit_id, 
+             -- C.uuid,
+             AA.a_id as aa_a_id, 
+             AA.action_id as aa_action_id, 
+             AA.level as aa_level, 
+             AA.value as aa_value, 
+             TM.name as tm_name, 
+             MA.qos as ma_qos, 
+             MA.warn as ma_warn, 
+             MA.fail as ma_fail, 
+             TM.display_name as tm_display_name, 
+             TM.unit as tm_unit, 
+             
+             TA.name as ta_name, 
+             TA.unit as ta_unit, 
+             AAP.a_id as aap_a_id, 
+             TAP.id as tap_id, 
+             AAP.value as aap_value, 
+             TAP.name as tap_name, 
+             TAP.unit as tap_unit 
+          FROM 
+             npm_dba.circuit_alert C 
+             join npm_dba.metric_alert MA on  MA.c_id = C.c_id 
+             join npm_dba.threshold_metric TM on TM.id = MA.metric_id 
+             JOIN npm_dba.action_alert AA ON MA.m_id = AA.m_id 
+             JOIN npm_dba.action_alert_parameter AAP ON AAP.a_id = AA.a_id 
+             JOIN npm_dba.threshold_action TA ON  TA.id = AA.action_id 
+             JOIN npm_dba.threshold_action_parameter TAP ON  TAP.id = AAP.parameter_id 
+             where circuit_id ='05/KEFN/102871/TWCS' 
+          ORDER BY ma_m_id, aa_a_id, aap_a_id, tap_id
